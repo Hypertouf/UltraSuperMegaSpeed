f1ce582209cea90f7f57e806f5804d96ff596f75
SunPi <biometala@gmail.com>
2025-08-18 19:54:58 +0200

    It was just a nightmare

--- main.gd
+++ main.gd
@@ -1,11 +1,15 @@
 extends Node3D
 @export var AnimPlayer : AnimationPlayer
 @export var Ui : Control
-@export var Alayer : VehicleBody3D
-
+@export var Player : VehicleBody3D
+@export var videofin : VideoStreamTheora
 
 func _on_finish_line_jeufinit() -> void:
-	print("ok j'essaies de l'ancer l'anim")	
-	AnimPlayer.play("Ending")
+	Ui.get_child(0).get_child(2).visible = true
 	
+	var VPlay = VideoStreamPlayer.new()
+	VPlay.stream = videofin
+	VPlay.autoplay = true
+	VPlay.position = Vector2(100,0)
+	Ui.get_child(0).get_child(2).add_child(VPlay)
 	pass # Replace with function body.
