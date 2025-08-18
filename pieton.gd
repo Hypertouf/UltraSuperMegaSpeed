3a4415ef57c947be3a0e293bf1f033231e4a3e05
SunPi <biometala@gmail.com>
2025-08-18 23:40:15 +0200

    commit to check if anything explodes (dialog added)

--- pieton.gd
+++ pieton.gd
@@ -5,9 +5,13 @@ extends Node3D
 @export var PietonMESH : Mesh
 @export var mesh : Node3D
 @export var explo : MeshInstance3D
+@export_enum("Good", "Bad", "Osef") var Karma: int
+
 
 func _ready() -> void:
 	mesh.mesh = PietonMESH
+	if Karma == 2 :
+		print("ok")
 	pass
 
 
