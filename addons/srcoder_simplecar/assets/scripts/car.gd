b11d2ca2784e9f5e731aadb8ea1c7798bf5981c7
seeyouinsamura <thomaspitavernier@gmail.com>
2026-05-07 17:46:46 +0200

    car2 first try

--- addons/srcoder_simplecar/assets/scripts/car.gd
+++ addons/srcoder_simplecar/assets/scripts/car.gd
@@ -77,7 +77,7 @@ func _physics_process(delta: float) -> void:
 		var actual_force : float = player_acceleration * ((-max_torque/max_wheel_rpm) * abs(wheel.get_rpm()) + max_torque) 
 		wheel.engine_force = actual_force
 	
-	followcamera.mycamera.fov = 75 + (abs(hypothenuse(linear_velocity.x, linear_velocity.z))) #adaptive FOV, when the car go faster the FOV go wider to make it feel faster.
+	#followcamera.mycamera.fov = 75 + (abs(hypothenuse(linear_velocity.x, linear_velocity.z))) #adaptive FOV, when the car go faster the FOV go wider to make it feel faster.
 																									   #the math used here is simply calculating the hypothenus of the triangle formed by the x axis vector and the z axis vector. this allows the fov adaptation to remain stable no matter the direction in which the car is turning.
 																									   # calculating the absolute of this hypothenus simply makes the fov NOT works backwards when the car is going negative x and z directions.
 
