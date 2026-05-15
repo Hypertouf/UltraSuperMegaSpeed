0a3ea9b9ce29cc4e959970b949f62bfaa35f0634
seeyouinsamura <thomaspitavernier@gmail.com>
2026-05-15 15:55:46 +0200

    DO YOU SEE THE TV GLOW ?

--- assets/Models/props/tv.gd
+++ assets/Models/props/tv.gd
@@ -1,8 +1,21 @@
 extends RigidBody3D
 
+@export var mesh : MeshInstance3D
+@export var anim : AnimationPlayer
+@export var intact : Mesh
+@export var broke : Mesh
 
-func _on_body_entered(body: Node) -> void:
-	if body.get_class() == "VehicleBody3D" :
-		print ("AAAAAH")
+
+func _ready() -> void:
+	anim.play("Idle_noise")
+	pass
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	
+	if area.name == "hitbox" :
+		anim.stop()
+		mesh.mesh = broke
 		pass
+	
 	pass # Replace with function body.
