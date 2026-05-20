287c194dea479497cc3b32ec45674b4d72e727e3
Hypertouf <bastien.souarn@gmail.com>
2026-05-20 13:19:24 +0200

    Children have been gifted a mouth, and they took a silly bite out of the yapple.

--- Scripts/pieton.gd
+++ Scripts/pieton.gd
@@ -4,9 +4,17 @@ extends Node3D
 @export var mesh : Node3D
 @export var explo : MeshInstance3D
 @export_enum("Good", "Bad", "Osef") var Karma: int
+@export var child_quotes : AudioStreamPlayer3D
 
 
 func _ready() -> void:
 	mesh.mesh = PietonMESH
-
+	$Timer.start(randf_range(0.5, 3.0))
 	pass
+
+func _process(delta: float) -> void:
+	if $Timer.is_stopped() :
+		child_quotes.play()
+		
+func _on_child_quotes_finished() -> void:
+	$Timer.start(randf_range(0.7, 3.0))
