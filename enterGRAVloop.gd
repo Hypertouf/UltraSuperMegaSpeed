a77974ea8ce4c65d609afc72999972d71f2ad962
Hypertouf <bastien.souarn@gmail.com>
2026-06-05 12:43:44 +0200

    skull emoji

--- enterGRAVloop.gd
+++ enterGRAVloop.gd
@@ -1,3 +1,18 @@
 extends Area3D
 
 class_name enterGRAVloop
+
+func _on_badtothebone_finished() -> void:
+	$badtothebone.play()
+
+
+func _on_badtothebone_2_finished() -> void:
+	$badtothebone2.play()
+
+
+func _on_badtothebone_3_finished() -> void:
+	$badtothebone3.play()
+
+
+func _on_badtothebone_4_finished() -> void:
+	$badtothebone4.play()
